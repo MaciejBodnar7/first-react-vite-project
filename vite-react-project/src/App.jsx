670a47fd602922c9ab09de7d28585a4a3399f6fc
MaciejBodnar7 <maciej.bodnar2@gmail.com>
2024-05-11 16:07:50 +0200

    styling

--- vite-react-project/src/App.jsx
+++ vite-react-project/src/App.jsx
@@ -7,11 +7,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <img className="logo" src={reactLogo} alt="react logo" />
-        <div className="ml-6 text-left">
-          <h1 className="text-4xl font-bold">Fun facts about react</h1>
-          <ul className="ml-6 mt-4">
+      <div className="">
+        <div className="div-main ml-6 text-left bg-slate-200 w-fit p-6 mt-6 rounded-2xl">
+          <div className="div-1 flex gap-3 items-center h-fit ">
+            <h1 className="text-4xl font-bold">Fun facts about react</h1>
+            <img className="mt-1" src={reactLogo} alt="react logo" />
+          </div>
+          <ul className="ml-6 mt-4 flex flex-col">
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 100K stars on GitHub</li>
--- vite-react-project/src/Header.jsx
+++ vite-react-project/src/Header.jsx
@@ -1,11 +1,15 @@
 import "./Header.css"
+import reactLogo from "./assets/react.svg"
 
 function Header() {
   return (
     <>
       <nav>
         <header className="w-full bg-slate-300 h-20 flex justify-between items-center">
-          <h1 className="ml-6 text-2xl font-black text-gray-900">StaticSite</h1>
+          <div className="ml-6 flex bg-slate-200 py-2 px-4 rounded-full">
+            <img src={reactLogo} alt="reactLogo" />
+            <h1 className="ml-2 text-2xl font-black text-gray-900">StaticSite</h1>
+          </div>
           <ul className="flex gap-6 h-full mr-10 items-center">
             <a href="">
               <li className="bg-slate-500 py-1 px-4 rounded-full text-white font-bold">Menu</li>
